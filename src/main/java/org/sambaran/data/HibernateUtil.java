8d1a670e2df390cf473edc19b86b98f1b4a4b796
Sambaran Hazra <sambaran2010@gmail.com>
2018-06-20 20:50:23 +0530

    Organize imports

--- src/main/java/org/sambaran/data/HibernateUtil.java
+++ src/main/java/org/sambaran/data/HibernateUtil.java
@@ -5,9 +5,6 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.sambaran.data.entities.AccountType;
-import org.sambaran.data.entities.User;
 
 public class HibernateUtil
 {
